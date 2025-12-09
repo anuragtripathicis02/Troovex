9b577aa093e21fa17ac67f39b109d8a7be32dccf
Anurag tripath <anurag.t@cisinlabs.com>
2025-12-09 13:55:57 +0530

    new chnages

---
+++ src/app/admin/onboarding/companySupplierScreen/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from 'react'
+import Home from '@/component/admin/onboarding/companySupplierScreen/Home';
+
+const page = () => {
+  return (
+    <Home/>
+  )
+}
+
+export default page
