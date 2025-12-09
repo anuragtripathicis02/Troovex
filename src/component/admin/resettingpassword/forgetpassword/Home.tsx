5c660d0ea0fc910f949862f606b5ddf16d1b2783
Rajucisin <raju.m@cisinlabs.com>
2025-12-09 20:13:54 +0530

    Rest-changes

---
+++ src/component/admin/resettingpassword/forgetpassword/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ForgetPassword from './ForgetPassword'
+import Header from '@/component/common/Header'
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <ForgetPassword />
+        </>
+    )
+}
+
+export default Home
