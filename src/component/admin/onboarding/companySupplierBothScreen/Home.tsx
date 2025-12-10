4b83930ffb4e51aca963a70d80d526cd89f0acd7
Anurag tripath <anurag.t@cisinlabs.com>
2025-12-10 15:50:17 +0530

    new changes

---
+++ src/component/admin/onboarding/companySupplierBothScreen/Home.tsx
@@ -0,0 +1,17 @@
+import Header from '@/component/common/Header'
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className='mt-82'>
+        {/* <RoleSelection /> */}
+        anurag t
+      </div>
+    </div>
+  )
+}
+
+export default Home
