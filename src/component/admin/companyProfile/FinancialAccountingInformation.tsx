cc5c8be1393c97cc39603eb122515a22b6c1d647
Anurag tripath <anurag.t@cisinlabs.com>
2026-03-16 12:41:01 +0530

    update code today

---
+++ src/component/admin/companyProfile/FinancialAccountingInformation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FinancialAccountingInformation = () => {
+  return (
+    <div>FinancialAccountingInformation</div>
+  )
+}
+
+export default FinancialAccountingInformation
